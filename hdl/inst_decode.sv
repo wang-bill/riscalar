f0517366cbbf8c1848ed002920edd9eb244b82ee
Bill Wang <wangbill@mit.edu>
2023-10-30 15:48:40 -0400

    started decode

--- hdl/inst_decode.sv
+++ hdl/inst_decode.sv
@@ -14,7 +14,31 @@ module instruction_decode
     output logic [11:0] imm_out
   )
   // instruction type 
-  
+  logic [6:0] opcode;
+  logic [2:0] funct3;
+  logic [6:0] funct7
+  logic [4:0] rs1;
+  logic [4:0] rs2;
+  logic [19:0] imm; // value may be different depending on instruction type
+
+  always_comb begin
+    opcode = instruction_in[6:0];
+    
+    if (opcode == 7'b0110011) begin // R-type
+
+    end else if (opcode == 7'b0010011) begin // I-type
+    end else if (opcode == 7'b0000011) begin // I-type
+    end else if (opcode == 7'b0100011) begin // S-type
+    end else if (opcode == 7'b1100011) begin // B-type
+    end else if (opcode == 7'b1101111) begin // J-type
+    end else if (opcode == 7'b1100111) begin // I-type
+    end else if (opcode == 7'b0110111) begin // U-type
+    end else if (opcode == 7'b0010111) begin // U-type
+    end else if (opcode == 7'b1110011) begin // I-type
+    end else begin
+      // invalid opcode, throw some sort of error
+    end
+  end
 
 endmodule
 
