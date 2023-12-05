80897d1ad01748f2f885e364dad88f00ac23e6cc
Bill Wang <wangbill@mit.edu>
2023-12-05 15:59:20 -0500

    alu

--- superscalar/hdl/alu.sv
+++ superscalar/hdl/alu.sv
@@ -6,7 +6,8 @@
 module alu(
     input wire clk_in,
     input wire rst_in,
-    input wire valid_in,
+    input wire valid_in, // high for 1 clock cycle
+    input wire read_in,
     input wire signed [31:0] rval1_in,
     input wire signed [31:0] rval2_in,
     input wire [3:0] aluFunc_in,
@@ -14,45 +15,46 @@ module alu(
 
     output logic signed [31:0] data_out,
     output logic ready_out,
-    output logic valid_out,
-    output logic busy_out
+    output logic valid_out // high until output is read
 );
 
     localparam STALL_DURATION = 25;
     logic [STALL_DURATION-1:0] stall;
     logic stall_done;
 
-    always_ff @(posedge clk_in) begin // artificial stall
+
+    always_ff @(posedge clk_in) begin
         if (rst_in) begin
             ready_out <= 0;
             valid_out <= 0;
-            busy_out <= 0;
-            stall_done <= 0; 
+            stall_done <= 0;
+            stall_can_start <= 0;
         end else begin
-            if (valid_in) begin
-                ready_out <= 0;
-                valid_out <= 0;
-                busy_out <= 1;
-                
+            if (valid_in) begin // should only hit this once
+                // start computation (stall)
                 stall <= 1;
                 stall_done <= 0;
-            end else begin
-                for (int i = 1; i < STALL_DURATION; i=i+1) begin
-                    stall[i] <= stall[i-1];
+                stall_can_start <= 1;
+            end
+
+            if (stall_can_start) begin
+                if (!stall_done) begin
+                    for (int i = 1; i < STALL_DURATION; i=i+1) begin
+                        stall[i] <= stall[i-1];
+                    end
+                    if (stall[STALL_DURATION-1] == 1) begin
+                        stall_done <= 1;
+                    end
+                end else begin
+                    if (read_in) begin
+                        valid_out <= 0;
+                        ready_out <= 1;
+                        stall_can_start <= 0;
+                    end else begin
+                        valid_out <= 1;
+                        ready_out <= 0;
+                    end
                 end
-            end
-
-            if (stall[STALL_DURATION-1] == 1) begin // stall is over
-                ready_out <= 1;
-                valid_out <= 1;
-                busy_out <= 0;
-                stall_done <= 1;
-            end
-
-            if (stall_done) begin
-                ready_out <= 1;
-                valid_out <= 0;
-                busy_out <= 0;
             end
         end
     end
