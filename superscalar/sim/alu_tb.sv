881b9575d2cc0f4f91c23e83ef43b14e14a49c2f
Catherine Tang <cathytang07@gmail.com>
2023-12-07 16:01:53 -0500

    include types header to tb

--- superscalar/sim/alu_tb.sv
+++ superscalar/sim/alu_tb.sv
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 `default_nettype none
 // typedef enum {Add, Sub, And, Or, Xor, Slt, Sltu, Sll, Srl, Sra} AluFunc; //10 AluFuncs
+`include "hdl/types.svh"
 
 module alu_tb();
 
