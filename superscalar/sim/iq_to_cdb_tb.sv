e85559d6ae73d6dda123bf2cbd6520c011c4e96e
Catherine Tang <cathytang07@gmail.com>
2023-12-10 13:25:52 -0500

    modified testbench

--- superscalar/sim/iq_to_cdb_tb.sv
+++ superscalar/sim/iq_to_cdb_tb.sv
@@ -6,8 +6,8 @@ module iq_to_cdb_tb();
   logic clk_in;
   logic [15:0] sw;
   logic [3:0] btn_in;
-  logic [31:0] instruction;
-  logic iq_valid;
+  // logic [31:0] instruction;
+  // logic iq_valid;
 
   logic [15:0] led_out;
   logic [2:0] rgb0_out, rgb1_out;
@@ -19,8 +19,8 @@ module iq_to_cdb_tb();
           ( .clk_100mhz(clk_in),
             .sw(sw),
             .btn(btn_in),
-            .instruction(instruction),
-            .iq_valid(iq_valid),
+            // .instruction(instruction),
+            // .iq_valid(iq_valid),
 
             .led(led_out),
             .rgb0(rgb0_out),
@@ -49,30 +49,30 @@ module iq_to_cdb_tb();
 
     for (int i = 0; i < 2*10**2; i=i+1) begin
       #10;
-      if (i == 10) begin
-        iq_valid = 1;
-        instruction = 32'h00158593;
-      end else if (i == 11) begin
-        instruction = 32'h00158593;
-      end else if (i == 12) begin
-        instruction = 32'h00158593;
-      end else if (i == 13) begin
-        instruction = 32'h00158593;
-      end else if (i == 14) begin
-        instruction = 32'h00158593;
-      end else if (i == 15) begin
-        instruction = 32'h00158593;
-      end else if (i == 16) begin
-        instruction = 32'h031807B3;
-      end else if (i == 17) begin
-        instruction = 32'h031807B3;
-      end else if (i == 18) begin
-        instruction = 32'h031807B3;
-      end else if (i == 19) begin
-        instruction = 32'h031807B3;
-      end else begin
-        iq_valid = 0;
-      end
+      // if (i == 10) begin
+      //   iq_valid = 1;
+      //   instruction = 32'h00158593;
+      // end else if (i == 11) begin
+      //   instruction = 32'h00158593;
+      // end else if (i == 12) begin
+      //   instruction = 32'h00158593;
+      // end else if (i == 13) begin
+      //   instruction = 32'h00158593;
+      // end else if (i == 14) begin
+      //   instruction = 32'h00158593;
+      // end else if (i == 15) begin
+      //   instruction = 32'h00158593;
+      // end else if (i == 16) begin
+      //   instruction = 32'h031807B3;
+      // end else if (i == 17) begin
+      //   instruction = 32'h031807B3;
+      // end else if (i == 18) begin
+      //   instruction = 32'h031807B3;
+      // end else if (i == 19) begin
+      //   instruction = 32'h031807B3;
+      // end else begin
+      //   iq_valid = 0;
+      // end
       $display("%d", led_out);
     end
 
