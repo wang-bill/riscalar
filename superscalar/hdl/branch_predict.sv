3e3449798f48f8759c317642fc04556f532dcc95
Bill Wang <wangbill@mit.edu>
2023-12-14 10:17:05 -0500

    changes from last night

--- superscalar/hdl/branch_predict.sv
+++ superscalar/hdl/branch_predict.sv
@@ -8,6 +8,18 @@ module branch_predict
     input wire signed [31:0] pc_in,
     output logic branch_taken_out
   );
-  assign branch_taken_out = 0;
+  
+  localparam LHT_DEPTH = 8;
+  localparam LP_DEPTH = 4;
+
+  localparam GLOBAL_DEPTH = 8;
+  localparam CHOICe_DEPTH = 4;
+
+  logic local_history_table 
+
+  always_ff @(posedge clk_in) begin
+    if (rst_in)
+  end
+
 endmodule
 `default_nettype wire
